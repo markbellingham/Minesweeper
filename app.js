40b5915317a4a4ddc33bc3523c36021cfff77621
Mark Bellingham <mark.bellingham@protonmail.ch>
2020-05-28 12:45:20 +0100

    event listener to detect bomb click | can detect how many bombs surround each square

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const grid = document.querySelector('.grid');
 let width = 10;
 let bombAmount = 20;
 let squares = [];
-let total = 0;
 
 // create board
 function createBoard() {
@@ -18,9 +17,41 @@ function createBoard() {
         square.classList.add(shuffledArray[i]);
         grid.appendChild(square);
         squares.push(square);
+
+        // normal click;
+        square.addEventListener('click', function(e) {
+            click(square);
+        });
+    }
+
+    // add numbers
+    for(let i = 0; i < squares.length; i++) {
+        let total = 0;
+        const isLeftEdge = i % width === 0;
+        const isRightEdge = i % width === width - 1;
+
+        if (squares[i].classList.contains('valid')) {
+            if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++;
+            if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++;
+            if(i > 10 && squares[i-width].classList.contains('bomb')) total++;
+            if(i > 11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb')) total++;
+            if(i < 98 && !isRightEdge && squares[i+1].classList.contains('bomb')) total++;
+            if(i < 90 && !isLeftEdge && squares[i-1+width].classList.contains('bomb')) total++;
+            if(i < 88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total++;
+            if(i < 89 && squares[i+width].classList.contains('bomb')) total++;
+            squares[i].setAttribute('data', total);
+
+        }
     }
 }
 createBoard();
+
+// click on square actions
+function click(square) {
+    if(square.classList.contains('bomb')) {
+        alert('Game Over!');
+    }
+}
 
 function shuffle(a) {
     for(let i = a.length - 1; i > 0; i--) {
